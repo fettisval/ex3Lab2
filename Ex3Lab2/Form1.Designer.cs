1c964200b3a88083c41d96292b36002ab18022a4
fetti <fetti@DESKTOP-VD54E8B>
2023-05-28 19:20:45 +0300

    Add project files.

---
+++ Ex3Lab2/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Ex3Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNr1 = new System.Windows.Forms.TextBox();
+            this.txtRez = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.operatieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuAdunare = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuScadere = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripSeparator();
+            this.txtNr2 = new System.Windows.Forms.TextBox();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtNr1
+            // 
+            this.txtNr1.Location = new System.Drawing.Point(372, 94);
+            this.txtNr1.Name = "txtNr1";
+            this.txtNr1.Size = new System.Drawing.Size(155, 31);
+            this.txtNr1.TabIndex = 0;
+            // 
+            // txtRez
+            // 
+            this.txtRez.Location = new System.Drawing.Point(372, 241);
+            this.txtRez.Name = "txtRez";
+            this.txtRez.Size = new System.Drawing.Size(155, 31);
+            this.txtRez.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(212, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Primul numar:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(189, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Al doilea numar:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(242, 247);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Rezultatul:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.operatieToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 42);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // operatieToolStripMenuItem
+            // 
+            this.operatieToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuAdunare,
+            this.toolStripMenuItem1,
+            this.menuScadere,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4,
+            this.toolStripMenuItem5});
+            this.operatieToolStripMenuItem.Name = "operatieToolStripMenuItem";
+            this.operatieToolStripMenuItem.Size = new System.Drawing.Size(126, 38);
+            this.operatieToolStripMenuItem.Text = "Operatie";
+            // 
+            // menuAdunare
+            // 
+            this.menuAdunare.Name = "menuAdunare";
+            this.menuAdunare.Size = new System.Drawing.Size(359, 44);
+            this.menuAdunare.Text = " +";
+            this.menuAdunare.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(356, 6);
+            // 
+            // menuScadere
+            // 
+            this.menuScadere.Name = "menuScadere";
+            this.menuScadere.Size = new System.Drawing.Size(359, 44);
+            this.menuScadere.Text = " -";
+            this.menuScadere.Click += new System.EventHandler(this.menuScadere_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(356, 6);
+            // 
+            // txtNr2
+            // 
+            this.txtNr2.Location = new System.Drawing.Point(372, 145);
+            this.txtNr2.Name = "txtNr2";
+            this.txtNr2.Size = new System.Drawing.Size(155, 31);
+            this.txtNr2.TabIndex = 7;
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(359, 44);
+            this.toolStripMenuItem4.Text = " *";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem4_Click);
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(359, 44);
+            this.toolStripMenuItem5.Text = " /";
+            this.toolStripMenuItem5.Click += new System.EventHandler(this.toolStripMenuItem5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNr2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRez);
+            this.Controls.Add(this.txtNr1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNr1;
+        private System.Windows.Forms.TextBox txtRez;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem operatieToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuAdunare;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem menuScadere;
+        private System.Windows.Forms.TextBox txtNr2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+    }
+}
+
---
+++ Ex3Lab2/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Ex3Lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            int numar1 = int.Parse(txtNr1.Text);
+            int numar2 = int.Parse(txtNr2.Text);
+
+            int rezultat = numar1 + numar2;
+            txtRez.Text = rezultat.ToString();
+        }
+
+        private void menuScadere_Click(object sender, EventArgs e)
+        {
+            int numar1 = int.Parse(txtNr1.Text);
+            int numar2 = int.Parse(txtNr2.Text);
+
+            int rezultat = numar1 - numar2;
+            txtRez.Text = rezultat.ToString();
+
+        }
+
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            int numar1 = int.Parse(txtNr1.Text);
+            int numar2 = int.Parse(txtNr2.Text);
+
+            int rezultat = numar1 * numar2;
+            txtRez.Text = rezultat.ToString();
+        }
+
+        private void toolStripMenuItem5_Click(object sender, EventArgs e)
+        {
+            int numar1 = int.Parse(txtNr1.Text);
+            int numar2 = int.Parse(txtNr2.Text);
+
+            int rezultat = numar1 / numar2;
+            txtRez.Text = rezultat.ToString();
+        }
+    }
+}
